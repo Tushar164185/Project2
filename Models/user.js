cb297e94962f7a387b012d8dace2e54f7e339cee
Tushar164185 <tg62500@gmail.com>
2024-03-28 17:39:57 +0530

    Add Project Files

---
+++ Models/user.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose");
+const LocalMongoose=require("passport-local-mongoose");
+const schema=mongoose.Schema;
+const signup=new schema({
+    email:{
+        type:String,
+        required:true
+    }
+})
+signup.plugin(LocalMongoose);
+module.exports=mongoose.model("users",signup);
